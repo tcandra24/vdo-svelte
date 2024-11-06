2d1dd2bf8fdcb8a550a671f28fd459b4629d6d39
Tito Candra <tcandra007@gmail.com>
2024-11-06 23:44:21 +0700

    Init VDO Svelte

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  import "../../css/app.css";
+  import Navbar from '../../components/auth/Navbar.svelte'
+  import FooterAuth from '../../components/auth/FooterAuth.svelte'
+
+  const registerBg2 = '../../assets/img/register_bg_2.png';
+</script>
+
+
+
+<div>
+  <Navbar />
+  <main>
+    <section class="relative w-full h-full py-40 min-h-screen">
+      <div
+        class="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
+        style="background-image: url({registerBg2});"
+      ></div>
+      <slot></slot>
+      <FooterAuth absolute="true" />
+    </section>
+  </main>
+</div>
